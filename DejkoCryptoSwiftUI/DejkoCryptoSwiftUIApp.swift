bc266edf808b7d8f9e26c9230ca8e7bd316b9b4a
Furkan Cemal Caliskan <furkanc.caliskan@icloud.com>
2022-10-16 14:40:47 +0300

    Project has been created

---
+++ DejkoCryptoSwiftUI/DejkoCryptoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DejkoCryptoSwiftUIApp.swift
+//  DejkoCryptoSwiftUI
+//
+//  Created by Furkan Cemal Çalışkan on 16.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct DejkoCryptoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
